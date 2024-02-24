cbacb4e69c79e913d8a0f5840c608e9036d4fc73
ssek69420 <kaikevix2233@gmail.com>
2024-02-23 21:49:23 -0300

    ABC

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
 export default function App() {
 
-  const nome = "João";
-  const sobreNome = "Santos";
+  const name = "me"
+
+  const surname = "me2"
 
   return(
     <div>
-        <h1>Hello World</h1>
-        <p>Desenvolvido por: {nome}</p>
-    </div>  
-  );
+      <h1>its me</h1>
+      <p>the great: {name + " " + surname} </p>
+    </div>
+  )
 
+//Another way to add the blank space
+//separating the name-surname
+//(but this one you have to edit the text so its bad)
 
+const name2 = "me"
+
+  const surname2 = " me2" // ----> added a space before 'me2'. actual braindead lmao
+
+  //return(
+   // <div>
+    //  <h1>its me</h1>
+   //   <p>the great: {name + surname} </p>
+  //  </div>
+ // )
 }
