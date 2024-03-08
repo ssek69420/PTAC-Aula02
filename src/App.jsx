e39e48cc58e6fae3e847e31ed382cda4d8d7f365
ssek69420 <kaikevix2233@gmail.com>
2024-03-08 15:45:35 -0400

    implemented css into the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
-import Footer from "./components/Footer"
+import Footer from "./components/Footer";
+import "./globals.css";
 
 export default function App() {
   return(
