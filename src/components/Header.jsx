e65b086de4a57a7b76597563e426f2ec65e39e95
ssek69420 <kaikevix2233@gmail.com>
2024-04-06 02:00:55 -0300

    removed header-separation
    read the fucking code

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,14 +4,12 @@ export default function Header(){
             <header className="cabecalho">
                 <a href="#"><h1 className="logo">WS</h1></a>
                 <div className="container-header-separation">
-                    <div className="header-separation">
                 <ul>
                     
                     <li>
                         <a href="#projects" className="headerproj">My proj.</a>
                     </li>
                 </ul>
-                    </div>
                 </div>
             </header>
         </div>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -15,12 +15,11 @@ export default function Main(){
     <section className="home">
         <div className="info">
             <h1>Insert random text</h1>
-            <p>Insert random little text (like your dick LOLLLL)</p>
+            <p>In the depths of the enchanted forest, where the shadows dance with whispers of forgotten tales, lies a secret known only to the moon and stars. It is said that within the heart of the forest, a hidden path reveals itself only to those who dare to listen to the song of the night. Legends speak of a lost treasure guarded by ancient spirits, waiting to be discovered by the bold and the brave. Will you venture into the darkness and unravel the mysteries that lie within, or will you let fear hold you back from your destiny?</p>
             <ul>
                 <li>
                     <a href="#" target="_blank">
                         <i className="fab fa-instagram"/>
-                        
                     </a>
                 </li>
 
